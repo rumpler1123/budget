1617a7c439cac89f4c85c56970f7ac481045b5e2
Imre Rumpler <32617070+rumpler1123@users.noreply.github.com>
2019-09-12 21:04:12 +0200

    Update minuszlista.js

--- app/minuszlista.js
+++ app/minuszlista.js
@@ -22,7 +22,7 @@ function minuszLista() {
 
 					html += `<tr class="${id}">
 				<td id="tdItem-${id}">${item}</td>
-				<td id="tdMoney-${id}">${numberWithCommas(money)} Ft.</td>
+				<td id="tdMoney-${id}">${numberWithCommas(money)} Ft</td>
 				<td>
 				<i class="item-edit fas fa-edit pr-1 text-warning"></i>
 
